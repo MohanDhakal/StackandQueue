fae8384b32e43c626d600e7a806c2db9e6e33830
Mohan Kumar Dhakal <37332114+MohanDhakal@users.noreply.github.com>
2021-01-21 18:28:14 +0545

    Update MyStack.java

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class MyStack {
+    //initializing an array
     static int stackArray[] = new int[6];
+    //position of the  current index
     static int top = -1;
 
     public static void main(String[] args) {
-        //intArry size is 11
+
         Scanner sc = new Scanner(System.in);
         int size = stackArray.length;
         System.out.println(size);
@@ -15,6 +17,7 @@ public class MyStack {
             System.out.println("Stack Overflow");
         }
         while (top < size - 1) {
+            //takes input from the user in the console
             int item = sc.nextInt();
             push(item);
         }
@@ -25,15 +28,17 @@ public class MyStack {
         }
         while (top > -1) {
             System.out.println(pop());
-            top--;
         }
     }
 
     public static void push(int item) {
+        //preincreement of top before pushing an item
+        //first increase the index and then push the item
         stackArray[++top] = item;
     }
 
     public static int pop() {
         return stackArray[top];
+        top--;
     }
 }
